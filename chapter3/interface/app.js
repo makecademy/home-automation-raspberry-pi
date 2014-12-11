2328f6d378bf394614dc0c14f0952e07efa6512c
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-12-11 08:46:59 +0100

    new concept

--- chapter3/interface/app.js
+++ chapter3/interface/app.js
@@ -1,5 +1,4 @@
 // Imports
-var sensorLib = require('node-dht-sensor');
 var express = require('express');
 var app = express();
 
@@ -12,46 +11,15 @@ app.use(express.static(__dirname + '/public'));
 // node-aREST
 var rest = require("arest")(app);
 rest.addDevice('xbee','/dev/ttyUSB0');
+rest.addDevice('wifi','localhost:3000');
 
 // Interface routes
-app.get('/interface', function(req, res){
+app.get('/', function(req, res){
   var devices = rest.getDevices();
   res.render('interface', {devices: devices});
 });
 
-// pi-aREST
-var piREST = require('pi-arest')(app);
-
-// Raspberry Pi name & ID
-piREST.set_id('1');
-piREST.set_name('my_RPi');
-
-// DHT sensor
-var dht_sensor = {
-    initialize: function () {
-        return sensorLib.initialize(11, 23);
-    },
-    read: function () {
-        var readout = sensorLib.read();
-        
-        piREST.variable('temperature',readout.temperature.toFixed(2));
-        piREST.variable('humidity', readout.humidity.toFixed(2));
-        
-        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
-            'humidity: ' + readout.humidity.toFixed(2) + '%');
-        setTimeout(function () {
-            dht_sensor.read();
-        }, 2000);
-    }
-};
-
-if (dht_sensor.initialize()) {
-    dht_sensor.read();
-} else {
-    console.warn('Failed to initialize sensor');
-}
-
 // Start server
-var server = app.listen(3000, function() {
+var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
 });
