55bd13edd3843655d98f3b6ddceb80ef45e1ec90
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-12-11 07:52:40 +0100

    chapter 3 added

---
+++ chapter2/app.js
@@ -0,0 +1,52 @@
+// Imports
+var sensorLib = require('node-dht-sensor');
+var express = require('express');
+var app = express();
+
+// View engine
+app.set('view engine', 'jade');
+
+// Set public folder
+app.use(express.static(__dirname + '/public'));
+
+// Interface routes
+app.get('/interface', function(req, res){
+  res.render('interface');
+});
+
+// pi-aREST
+var piREST = require('pi-arest')(app);
+
+// Raspberry Pi name & ID
+piREST.set_id('1');
+piREST.set_name('my_RPi');
+
+// DHT sensor
+var dht_sensor = {
+    initialize: function () {
+        return sensorLib.initialize(11, 23);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+        
+        piREST.variable('temperature',readout.temperature.toFixed(2));
+        piREST.variable('humidity', readout.humidity.toFixed(2));
+        
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        setTimeout(function () {
+            dht_sensor.read();
+        }, 2000);
+    }
+};
+
+if (dht_sensor.initialize()) {
+    dht_sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
+
+// Start server
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
---
+++ chapter3/pir_xbee/pir_xbee.ino
@@ -0,0 +1,26 @@
+// Libraries
+#include <SPI.h>
+#include <aREST.h>
+
+// Motion sensor ID
+char * xbee_id = "1";
+
+// Create ArduREST instance
+aREST rest = aREST();
+
+void setup() {
+  
+  // Start Serial
+  Serial.begin(9600);
+  
+  // Give name and ID to device
+  rest.set_id(xbee_id);
+  rest.set_name("motion1");
+}
+
+void loop() {
+  
+  // Handle REST calls
+  rest.handle(Serial);
+  
+}
