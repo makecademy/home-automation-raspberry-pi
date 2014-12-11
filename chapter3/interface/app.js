c3e461f9b39eca8593124491c00cc58eead659ae
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-12-11 09:06:25 +0100

    fixes

--- chapter3/interface/app.js
+++ chapter3/interface/app.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 
 // node-aREST
 var rest = require("arest")(app);
-rest.addDevice('xbee','/dev/ttyUSB0');
+//rest.addDevice('xbee','/dev/ttyUSB0');
 rest.addDevice('http','192.168.1.104');
 
 // Interface routes
