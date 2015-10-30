dabc2300859ab4e790726fc148ad444dda0cdf65
marcoschwartz <marcolivier.schwartz@gmail.com>
2015-10-30 13:43:50 +0100

    added lamp control with push button

---
+++ lamp_control/app.js
@@ -0,0 +1,24 @@
+// Imports
+var gpio = require('rpi-gpio');
+
+// Lamp state
+var lampState = false;
+
+// Listen for events
+gpio.on('change', function(channel, value) {
+    console.log('Channel ' + channel + ' value is now ' + value);
+
+    // If button is pressed, change the status of the lamp
+    if (value == true) {
+      lampState = !lampState;
+      gpio.setup(7, gpio.DIR_OUT, function() {
+        gpio.write(7, lampState, function(err) {
+          if (err) throw err;
+          console.log('Switch!');
+         });
+      });
+    }
+});
+
+// Setup pin 23 as input
+gpio.setup(23, gpio.DIR_IN, gpio.EDGE_BOTH);
